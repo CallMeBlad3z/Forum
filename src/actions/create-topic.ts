fb795a83f1efb1049d4e84a66d6caabbae2c5864
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-06 22:46:55 +0300

    Update create-topic.ts
    Made a simple validation check and test for the validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,25 @@
 'use server';
 
-export async function createTopic() {
+import { z } from 'zod';
 
+const createTopicSchema = z.object({
+    name: z
+    .string()
+    .min(10)
+    .regex(/^[a-zA-Z- ]+$/, { 
+        message: 'Must be lowercase letter or dashes without spaces.'
+    }),
+    description: z.string().min(10)
+});
+
+export async function createTopic(formData: FormData) {
+    const result = createTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+    });
+
+    if (!result.success) {
+        console.log(result.error.flatten().fieldErrors)
+    }
     //TODO: revalidate the homepage
 }
