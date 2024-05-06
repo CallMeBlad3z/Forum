392ec214e7666eb2760b579bcae0256d6161b1a6
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-06 21:38:09 +0300

    Update topic-create-form.tsx

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,6 +1,33 @@
-
+import {
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+} from '@nextui-org/react'
 
 
 export default function TopicCreateForm() {
-    return <div>Topic Create Form</div>;
+    return (
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Button color="primary">Create Topic</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form>
+                    <div className="flex flex-col gap-4 p-4 w-80">
+                        <h3 className="text-lg">Create a Topic</h3>
+                        <Input label="Name" labelPlacement="outside" placeholder="Name" />
+                        <Textarea 
+                            label="Description"
+                            labelPlacement="outside"
+                            placeholder="Describe your topic"
+                            />
+                        <Button color="success" type="submit">Create</Button>
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
 }
