7978c5d2c3e36dc1b0b2c395d72e9aab5cbb4ebb
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2024-05-15 17:32:47 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,28 @@
 import Link from 'next/link';
-import { 
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Input,
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
 } from '@nextui-org/react';
-import HeaderAuth from '@/components/header-auth'
+import HeaderAuth from '@/components/header-auth';
+import SearchInput from '@/components/search-input';
 
 export default function Header() {
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">DiscussHub</Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <SearchInput />
+        </NavbarItem>
+      </NavbarContent>
 
-    return (
-        <Navbar className="shadow mb-6">
-            <NavbarBrand>
-                <Link href="/" className="font-bold">DiscussHub</Link>
-            </NavbarBrand>
-            <NavbarContent justify="center">
-                <NavbarItem>
-                    <Input />
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <HeaderAuth />
-            </NavbarContent>
-        </Navbar>
-    );
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
 }
